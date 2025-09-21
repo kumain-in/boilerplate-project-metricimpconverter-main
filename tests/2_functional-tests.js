0dfd270c1ee7424e89b16884a551fbea16642826
kumain-in <kumain.in@outlook.com>
2025-09-21 15:01:17 +0700

    up

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -19,7 +19,7 @@ suite('Functional Tests', function() {
           assert.equal(res.status, 200);
           assert.equal(res.body.initNum, 10);
           assert.equal(res.body.initUnit, 'L');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
+          assert.equal(res.body.returnNum, 2.64172);
           assert.equal(res.body.returnUnit, 'gal');
           done();
         });
@@ -66,7 +66,7 @@ suite('Functional Tests', function() {
            assert.equal(res.status, 200);
            assert.equal(res.body.initNum, 1);
            assert.equal(res.body.initUnit, 'kg');
-           assert.approximately(res.body.returnNum, 2.20462, 0.1);
+           assert.equal(res.body.returnNum, 2.20462);
            assert.equal(res.body.returnUnit, 'lbs');
            done();
          });
